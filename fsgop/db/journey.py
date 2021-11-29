1f2feae8018bb6fa5c84c88f41d62476fb889eb4
claashk <claashk@gmx.de>
2021-11-29 09:12:30 +0100

    * Added type annotations to Journey and Vehicle * Updated almost_equal comparison of Journeys

--- fsgop/db/journey.py
+++ fsgop/db/journey.py
@@ -1,5 +1,7 @@
-from .record import Record, to
 from datetime import datetime, timedelta
+from typing import Union, Optional, Set
+
+from .record import Record, to
 from .vehicle import Vehicle
 from .person import Person
 from .launch_method import LaunchMethod
@@ -19,60 +21,60 @@ CHARGE_PILOT_AND_COPILOT = 3
 class Journey(Record):
     """Native data model for a flight or other journey of a vehicle
     
-    Arguments:
-        uid (int): Unique numeric ID of this journey.
-        vehicle_uid (int): Unique numeric ID of vehicle used for this journey
-           (ref. :doc:`Vehicle`)
-        pilot_uid (int): Unique numeric ID of the person in charge of the
-            vehicle operation (PIC for regular flights, student pilot for two
-            seated training).
-        copilot_uid (int): Unique numeric ID of the second crew member or
-            instructor for two-seated training.
-        passenger1_uid (int): Unique numeric ID of first passenger
-        passenger2_uid (int): Unique numeric ID of second passenger
-        passenger3_uid (int): Unique numeric ID of third passenger
-        passenger4_uid (int): Unique numeric ID of fourth passenger
-        category (str): Journey category
-        num_trips (int): Number of landings/winch launches
-        launch_method_uid (int): ID of applicable :class:`.LaunchMethod` record
-           in table ``launch_methods`` (ref. :doc:`launch_method`)
-        launch_uid (int): UID of tow-flight or winch mission period.
-        departure_location (str): Departure location
-        departure_utc (datetime.datetime): Departure time
-        arrival_location (str): Landing location
-        arrival_utc (datetime.datetime): Landing time
-        off_block_utc (datetime.datetime): Offblock UTC
-        on_block_utc (datetime.datetime): On-block UTC
-        engine_hours_upon_departure(datetime.timedelta): Engine hours (Hobbs meter reading)
-        engine_hours_upon_arrival(datetime.timedelta): Engine hours upon arrival
-        charge_person_uid (int): uid of member to charge or a negative constant
+    Args:
+        uid: Unique numeric ID of this journey.
+        vehicle: Vehicle used for this journey. An integer will be interpreted as
+          Vehicle uid.
+        pilot: Person in charge of vehicle operation (PIC for regular flights,
+          student pilot for two seated training). An integer will be interpreted
+          as Person uid.
+        copilot: Second crew member or instructor for two-seated training. An
+          integer will be interpreted as Person uid.
+        passenger1: Passenger
+        passenger2: Passenger
+        passenger3: Passenger
+        passenger4: Passenger
+        category: Journey category
+        num_trips: Number of landings/winch launches
+        launch_method: Launch method. An integer will be interpreted as uid.
+        launch_journey: Tow-flight or winch mission period.
+        departure_location: Departure location
+        departure_utc: Departure time
+        arrival_location: Landing location
+        arrival_utc: Landing time
+        off_block_utc: Offblock UTC
+        on_block_utc: On-block UTC
+        engine_hours_upon_departure: Engine hours (Hobbs meter reading)
+        engine_hours_upon_arrival: Engine hours upon arrival
+        charge_person: Member to charge or a negative constant
         comments (str): Eventual comments
     """
-    index = ["vehicle_uid", "departure_utc", "arrival_utc"]
+    index = ["vehicle", "departure_utc", "arrival_utc"]
 
-    def __init__(self,
-                 uid=None,
-                 vehicle=None,
-                 pilot=None,
-                 copilot=None,
-                 passenger1=None,
-                 passenger2=None,
-                 passenger3=None,
-                 passenger4=None,
-                 category=None,
-                 num_trips=None,
-                 launch_method=None,
-                 launch_journey=None,
-                 departure_location=None,
-                 departure_utc=None,
-                 arrival_location=None,
-                 arrival_utc=None,
-                 off_block_utc=None,
-                 on_block_utc=None,
-                 engine_hours_upon_departure=None,
-                 engine_hours_upon_arrival=None,
-                 charge_person=None,
-                 comments=None):
+    def __init__(
+            self,
+            uid: Optional[int] = None,
+            vehicle: Optional[Union[int, Vehicle]] = None,
+            pilot: Optional[Union[int, Person]] = None,
+            copilot: Optional[Union[int, Person]] = None,
+            passenger1: Optional[Union[int, Person]] = None,
+            passenger2: Optional[Union[int, Person]] = None,
+            passenger3: Optional[Union[int, Person]] = None,
+            passenger4: Optional[Union[int, Person]] = None,
+            category: Optional[str] = None,
+            num_trips: Optional[int] = None,
+            launch_method: Optional[Union[int, LaunchMethod]] = None,
+            launch_journey: Optional[Union[int, "Journey"]] = None,
+            departure_location: Optional[str] = None,
+            departure_utc: Optional[Union[str, datetime]] = None,
+            arrival_location: Optional[str] = None,
+            arrival_utc: Optional[Union[str, datetime]] = None,
+            off_block_utc: Optional[Union[str, datetime]] = None,
+            on_block_utc: Optional[Union[str, datetime]] = None,
+            engine_hours_upon_departure: Optional[Union[str, timedelta]] = None,
+            engine_hours_upon_arrival: Optional[Union[str, timedelta]] = None,
+            charge_person: Optional[Union[int, Person]] = None,
+            comments=None) -> None:
         super().__init__(uid=uid)
         self.vehicle = to(Vehicle, vehicle)
         self.pilot = to(Person, pilot)
@@ -83,97 +85,66 @@ class Journey(Record):
         self.passenger4 = to(Person, passenger4, default=None)
         self.category = to(int, category, default=NORMAL_FLIGHT)
         self.num_trips = to(int, num_trips, default=1)
-        self.launch_method = to(LaunchMethod, launch_method, default=None)
-        self.launch_journey = to(Journey, launch_journey, default=None)
+        self.launch_method = to(LaunchMethod, launch_method)
+        self.launch_journey = to(Journey, launch_journey)
         self.departure_location = to(str, departure_location)
         self.departure_utc = to(datetime, departure_utc)
         self.arrival_location = to(str, arrival_location)
         self.arrival_utc = to(datetime, arrival_utc, default=None)
         self.off_block_utc = to(datetime, off_block_utc, default=None)
         self.on_block_utc = to(datetime, on_block_utc, default=None)
-        self.engine_hours_upon_departure = to(timedelta, engine_hours_upon_departure, default=None)
-        self.engine_hours_upon_arrival = to(timedelta, engine_hours_upon_arrival, default=None)
+        self.engine_hours_upon_departure = to(timedelta,
+                                              engine_hours_upon_departure,
+                                              default=None)
+        self.engine_hours_upon_arrival = to(timedelta,
+                                            engine_hours_upon_arrival,
+                                            default=None)
         self.charge_person = to(Person, charge_person, default=self.pilot)
-        self.comments = comments
+        self.comments = to(str, comments, default=None)
 
-    def pic(self):
-        """Get pilot in command
-
-        Return:
-            int: ID of pilot in command
+    def pic(self) -> Person:
+        """Returns pilot in command
         """
         if self.category == TWO_SEATED_TRAINING:
             return self.copilot
         return self.pilot
+
+    def crew(self) -> Set[Person]:
+        """Returns the crew of this journey as set"""
+        return {self.pilot,
+                self.copilot,
+                self.passenger1,
+                self.passenger2,
+                self.passenger3,
+                self.passenger4} - {Person()}
         
-    def duration(self):
-        """Get flight time
-        
-        Return:
-            datetime.timedelta : Flight time
+    def duration(self) -> timedelta:
+        """Returns the duration of the journey
         """
         return self.arrival_utc - self.departure_utc
 
-    def departure_time_str(self, format=None):
-        """Get departure time as string.
-        
-        Arguments:
-            format (str): Output format (in :func:`strftime` notation).
-               Defaults to :data:`~pysk.db.model.flight.TIME_FORMAT`.
-        
-        Return:
-            str: Departure time as string in given format for local and outbound
-            flights. Departure location for inbound flights.
-        """
-        return self.time_to_str(self.departure_time, fmt=format)
-
-    def landing_time_str(self, format=None):
-        """Get landing time as string.
-        
-        Arguments:
-            format (str): Output format (in :func:`strftime` notation).
-               Defaults to :data:`~pysk.flight.TIME_FORMAT`.
-        
-        Return:
-            str: Landing time as string in given format for local and inbound
-            flights. Landing location for outbound flights.
-        """
-        return self.time_to_str(self.landing_time, fmt=format)
-
-    def conflicts_with(self, other):
+    def almost_equal(self, other: "Journey") -> bool:
         """Check if two flights are conflicting.
         
         Two flights are similar, if and only if their flight times overlap and
         either a crew member or the plane are identical.
         
-        Arguments:
-            other (:class:`Journey`): flight to compare to
+        Args:
+            other: Journey to compare to
         
-        Return:
-            ``True`` if and only if self and other are conflicting
+        Returns:
+            ``True`` if and only if `self` and `other` are similar
         """
-        if None in (self.arrival_utc,
-                    self.departure_utc,
-                    self.vehicle_uid,
-                    self.pilot_uid):
-            raise RuntimeError(f"{self} is incomplete")
-        
-        if None in (other.arrival_utc,
-                    other.departure_utc,
-                    other.vehicle_uid,
-                    other.pilot_uid):
-            raise RuntimeError(f"{other} is incomplete")
+        for x in (self, other):
+            if None in (x.arrival_utc,
+                        x.departure_utc,
+                        x.vehicle,
+                        x.pilot):
+                raise ValueError(f"{x} is incomplete")
 
         return all((
             self.arrival_utc >= other.departure_utc,
             self.departure_utc <= other.arrival_utc,
-            any((self.pilot_uid == other.pilot_uid,
-                 self.vehicle_uid == other.vehicle_uid,
-                 self.copilot_uid is not None
-                 and self.copilot_uid == other.copilot_uid))))
-
-    @staticmethod
-    def time_to_str(t, fmt=None):
-        if t is None:
-            return ""
-        return t.strftime(TIME_FORMAT if fmt is None else fmt)
+            any((bool(self.crew() & other.crew()),
+                 self.vehicle == other.vehicle))
+        ))
--- fsgop/db/vehicle.py
+++ fsgop/db/vehicle.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from .record import Record, to
 
 SINGLE_ENGINE_PISTON = 1
@@ -13,29 +14,29 @@ CAR = 12
 class Vehicle(Record):
     """Native vehicle model
     
-    Arguments:
-        uid (int): Unique numeric ID of this vehicle record
-        manufacturer (str): Name of airplane manufacturer
-        model (str): Model of airplane
-        serial (str): Unique airplane serial number
-        num_seats (int): Number of seats
-        category (int): Category. One of the values in CATEGORIES.
-        comments (str): any comment
+    Args:
+        uid: Unique numeric ID of this vehicle record
+        manufacturer: Name of airplane manufacturer
+        model: Model of airplane
+        serial: Unique airplane serial number
+        num_seats: Number of seats
+        category: Category. One of the values in CATEGORIES.
+        comments: any comment
     """
     index = ["manufacturer", "serial"]
 
     def __init__(self,
-                 uid=None,
-                 manufacturer=None,
-                 model=None,
-                 serial=None,
-                 num_seats=None,
-                 category=None,
-                 comments=None):
+                 uid: Optional[int] = None,
+                 manufacturer: Optional[str] = None,
+                 model: Optional[str] = None,
+                 serial: Optional[str] = None,
+                 num_seats: Optional[int] = None,
+                 category: Optional[int] = None,
+                 comments: Optional[str] = None) -> None:
         super().__init__(uid=uid)
-        self.manufacturer = to(str, manufacturer, default="").strip()
-        self.model = to(str, model, default="").strip()
-        self.serial = to(str, serial, default="").strip()
+        self.manufacturer = to(str, manufacturer, default=None)
+        self.model = to(str, model, default=None)
+        self.serial = to(str, serial, default=None)
         self.num_seats = to(int, num_seats, default=1)
         self.category = to(int, category, default=None)
-        self.comments = to(str, comments, default="").strip()
+        self.comments = to(str, comments, default=None)
