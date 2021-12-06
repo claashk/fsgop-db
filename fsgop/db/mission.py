fc49c4b7a000054dc1e2a59663514b667a8c7456
claashk <claashk@gmx.de>
2021-12-06 16:25:09 +0100

    Renamed Journey to Mission

--- fsgop/db/mission.py
+++ fsgop/db/mission.py
@@ -4,10 +4,10 @@ from typing import Union, Optional, Set
 from .record import Record, to
 from .vehicle import Vehicle
 from .person import Person
-from .launch_method import LaunchMethod
 
 NORMAL_FLIGHT = 1
 GUEST_FLIGHT = 2
+CHECK_FLIGHT = 3
 ONE_SEATED_TRAINING = 11
 TWO_SEATED_TRAINING = 12
 WINCH_OPERATION = 21
@@ -18,12 +18,12 @@ CHARGE_GUEST = 3
 CHARGE_PILOT_AND_COPILOT = 3
 
 
-class Journey(Record):
-    """Native data model for a flight or other journey of a vehicle
+class Mission(Record):
+    """Native data model for a flight, journey or other mission of a vehicle
     
     Args:
         uid: Unique numeric ID of this journey.
-        vehicle: Vehicle used for this journey. An integer will be interpreted as
+        vehicle: Vehicle used for this mission. An integer will be interpreted as
           Vehicle uid.
         pilot: Person in charge of vehicle operation (PIC for regular flights,
           student pilot for two seated training). An integer will be interpreted
@@ -34,22 +34,21 @@ class Journey(Record):
         passenger2: Passenger
         passenger3: Passenger
         passenger4: Passenger
-        category: Journey category
-        num_trips: Number of landings/winch launches
-        launch_method: Launch method. An integer will be interpreted as uid.
-        launch_journey: Tow-flight or winch mission period.
-        departure_location: Departure location
-        departure_utc: Departure time
-        arrival_location: Landing location
-        arrival_utc: Landing time
-        off_block_utc: Offblock UTC
+        category: Mission category
+        num_stints: Number of landings/winch launches
+        launch: Tow-flight or winch mission period.
+        origin: Departure location
+        begin: Departure time (UTC)
+        destination: Landing location
+        end: Landing time (UTC)
+        off_block_utc: Off-block UTC
         on_block_utc: On-block UTC
-        engine_hours_upon_departure: Engine hours (Hobbs meter reading)
-        engine_hours_upon_arrival: Engine hours upon arrival
+        engine_hours_begin: Engine hours (Hobbs meter reading)
+        engine_hours_end: Engine hours upon arrival
         charge_person: Member to charge or a negative constant
-        comments (str): Eventual comments
+        comments: Eventual comments
     """
-    index = ["vehicle", "departure_utc", "arrival_utc"]
+    index = ["begin", "vehicle"]
 
     def __init__(
             self,
@@ -62,17 +61,16 @@ class Journey(Record):
             passenger3: Optional[Union[int, Person]] = None,
             passenger4: Optional[Union[int, Person]] = None,
             category: Optional[str] = None,
-            num_trips: Optional[int] = None,
-            launch_method: Optional[Union[int, LaunchMethod]] = None,
-            launch_journey: Optional[Union[int, "Journey"]] = None,
-            departure_location: Optional[str] = None,
-            departure_utc: Optional[Union[str, datetime]] = None,
-            arrival_location: Optional[str] = None,
-            arrival_utc: Optional[Union[str, datetime]] = None,
+            num_stints: Optional[int] = None,
+            launch: Optional[Union[int, "Mission"]] = None,
+            origin: Optional[str] = None,
+            begin: Optional[Union[str, datetime]] = None,
+            destination: Optional[str] = None,
+            end: Optional[Union[str, datetime]] = None,
             off_block_utc: Optional[Union[str, datetime]] = None,
             on_block_utc: Optional[Union[str, datetime]] = None,
-            engine_hours_upon_departure: Optional[Union[str, timedelta]] = None,
-            engine_hours_upon_arrival: Optional[Union[str, timedelta]] = None,
+            engine_hours_begin: Optional[Union[str, timedelta]] = None,
+            engine_hours_end: Optional[Union[str, timedelta]] = None,
             charge_person: Optional[Union[int, Person]] = None,
             comments=None) -> None:
         super().__init__(uid=uid)
@@ -84,21 +82,20 @@ class Journey(Record):
         self.passenger3 = to(Person, passenger3, default=None)
         self.passenger4 = to(Person, passenger4, default=None)
         self.category = to(int, category, default=NORMAL_FLIGHT)
-        self.num_trips = to(int, num_trips, default=1)
-        self.launch_method = to(LaunchMethod, launch_method)
-        self.launch_journey = to(Journey, launch_journey)
-        self.departure_location = to(str, departure_location)
-        self.departure_utc = to(datetime, departure_utc)
-        self.arrival_location = to(str, arrival_location)
-        self.arrival_utc = to(datetime, arrival_utc, default=None)
+        self.num_stints = to(int, num_stints, default=1)
+        self.launch = to(Mission, launch)
+        self.origin = to(str, origin)
+        self.begin = to(datetime, begin)
+        self.destination = to(str, destination)
+        self.end = to(datetime, end, default=None)
         self.off_block_utc = to(datetime, off_block_utc, default=None)
         self.on_block_utc = to(datetime, on_block_utc, default=None)
-        self.engine_hours_upon_departure = to(timedelta,
-                                              engine_hours_upon_departure,
-                                              default=None)
-        self.engine_hours_upon_arrival = to(timedelta,
-                                            engine_hours_upon_arrival,
-                                            default=None)
+        hrs = []
+        for x in (engine_hours_begin, engine_hours_end):
+            dt = to(timedelta, x, default=None)
+            if dt is not None:
+                hrs.append(dt.total_seconds() / 3600.)
+        self.engine_hours_begin, self.engine_hours_end = hrs
         self.charge_person = to(Person, charge_person, default=self.pilot)
         self.comments = to(str, comments, default=None)
 
@@ -121,9 +118,9 @@ class Journey(Record):
     def duration(self) -> timedelta:
         """Returns the duration of the journey
         """
-        return self.arrival_utc - self.departure_utc
+        return self.end - self.begin
 
-    def almost_equal(self, other: "Journey") -> bool:
+    def almost_equal(self, other: "Mission") -> bool:
         """Check if two flights are conflicting.
         
         Two flights are similar, if and only if their flight times overlap and
@@ -136,15 +133,15 @@ class Journey(Record):
             ``True`` if and only if `self` and `other` are similar
         """
         for x in (self, other):
-            if None in (x.arrival_utc,
-                        x.departure_utc,
+            if None in (x.end,
+                        x.begin,
                         x.vehicle,
                         x.pilot):
                 raise ValueError(f"{x} is incomplete")
 
         return all((
-            self.arrival_utc >= other.departure_utc,
-            self.departure_utc <= other.arrival_utc,
+            self.end >= other.begin,
+            self.begin <= other.end,
             any((bool(self.crew() & other.crew()),
                  self.vehicle == other.vehicle))
         ))
